892f8fdaca3d7a0854cc12e44b79d806b47d32b0
Kerry Rodden <kerryrodden@users.noreply.github.com>
2018-05-30 16:16:55 -0700

    test: ensure entries are retrieved and displayed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -78,6 +78,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -85,6 +86,17 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('are retrieved and displayed correctly', function(done) {
+            expect(document.querySelectorAll('.feed .entry').length).toBeGreaterThan(0);
+            done();
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
